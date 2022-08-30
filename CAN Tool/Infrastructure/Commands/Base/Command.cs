be7bdf243bf257e8e5fc9e93949893652c00208b
EXEcutorXS <73341778+EXEcutorXS@users.noreply.github.com>
2022-08-30 13:39:58 +0400

    Structure improved

---
+++ CAN Tool/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CAN_Tool.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+        
+
+        public abstract void Execute(object parameter);
+        
+    }
+}
---
+++ CAN Tool/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAN_Tool.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (field.Equals(value))
+            {
+                field = value;
+                OnPropertyChanged(PropertyName);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose()
+        { 
+        Dispose(true);
+        }
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+            //Освобождение занятых ресурсов
+            return;
+        }
+
+    }
+}
