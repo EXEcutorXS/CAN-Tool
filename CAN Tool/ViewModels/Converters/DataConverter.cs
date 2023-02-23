de2aa76135f8ecf109b04ac38750a2c008ca68cc
Андрей Андрей <streamline88@inbox.ru>
2023-02-23 11:19:34 +0400

    Hiding zones replaced with opacity

--- CAN Tool/ViewModels/Converters/DataConverter.cs
+++ CAN Tool/ViewModels/Converters/DataConverter.cs
@@ -177,4 +177,20 @@ namespace CAN_Tool.ViewModels.Converters
         }
     }
 
+    public class BoolToOpacity : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && (bool)value == true)
+                return 1;
+            else
+                return 0.4;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("This is one way converter!");
+        }
+    }
+
 }
