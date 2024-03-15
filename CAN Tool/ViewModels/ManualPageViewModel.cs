b976ba6a0eb4ecf08fa49270bc3472be5946e1dd
EXEcutor <73341778+EXEcutorXS@users.noreply.github.com>
2024-03-15 11:27:10 +0400

    Manual page design fix

--- CAN Tool/ViewModels/ManualPageViewModel.cs
+++ CAN Tool/ViewModels/ManualPageViewModel.cs
@@ -24,6 +24,7 @@ namespace CAN_Tool.ViewModels
         [ObservableProperty] private int manualFuelPump;
         [ObservableProperty] private int manualGlowPlug;
         [ObservableProperty] private bool manualWaterPump;
+        private bool togglePumpRequest;
 
 
         [RelayCommand]
@@ -73,7 +74,7 @@ namespace CAN_Tool.ViewModels
         [RelayCommand]
         private void ToggleWaterPump(object parameter)
         {
-            ManualWaterPump = !ManualWaterPump;
+            togglePumpRequest = true;
             updateManualMode();
         }
 
@@ -89,7 +90,7 @@ namespace CAN_Tool.ViewModels
             msg.Data = new byte[8];
             msg.Data[0] = 0;
             msg.Data[1] = 68;
-            msg.Data[2] = (byte)(ManualWaterPump ? 1 : 0);
+            msg.Data[2] = (byte)((ManualWaterPump^togglePumpRequest)? 1 : 0);
             msg.Data[3] = (byte)ManualAirBlower;
             msg.Data[4] = (byte)ManualGlowPlug;
             msg.Data[5] = (byte)(ManualFuelPump / 256);
